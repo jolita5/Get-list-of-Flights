3b3766f42eaea9e4b46df84976c975a464a06657
DESKTOP-6KJ0KT3\Jolita <jolita.jezukeviciute@gmail.com>
2019-01-30 18:59:54 +0200

    Console aplication created. Uri adress added.

---
+++ Flights/Program.cs
@@ -0,0 +1,40 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flights
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GetHtmlAsync();
+
+            Console.ReadLine();
+        }
+
+        private static async void GetHtmlAsync()
+        {
+            var url = "https://newmood.lt/moterims/products?sort=-latest&new=true&ap=1%2C47";
+
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+
+            var flightsList = htmlDocument.DocumentNode.Descendants("ul")
+            .Where(node => node.GetAttributeValue("id", "")
+            .Equals("available-flight")).ToList();
+
+
+            Console.WriteLine();
+        }
+    }
+}
