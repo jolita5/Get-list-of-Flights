51615bd0b3c349d5056746430783995ef8246dbd
DESKTOP-6KJ0KT3\Jolita <jolita.jezukeviciute@gmail.com>
2019-02-01 21:36:45 +0200

    Dresses are printed as a list, 60 vnt

--- Flights/Program.cs
+++ Flights/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Flights
@@ -12,17 +13,17 @@ namespace Flights
     {
         static void Main(string[] args)
         {
-           int pageNr = 0;
-           GetHtmlAsync(pageNr);
+            int pageNr = 0;
+            GetHtmlAsync(pageNr);
 
-     
+
 
             Console.ReadLine();
         }
 
         private static async void GetHtmlAsync(int pageNr)
         {
-            var url = "https://newmood.lt/moterims/c/moterims-drabuziai-sukneles?ap=1%2C48%2C49%2C50";
+            var url = "https://newmood.lt/moterims/c/moterims-drabuziai-sukneles?page=1&sort=&ap=1%2C48%2C49%2C50";
 
 
             if (pageNr != 0)
@@ -52,21 +53,23 @@ namespace Flights
 
             foreach (var item in productList)
             {
-                Console.WriteLine(item.GetAttributeValue("col-xs-6 col-md-4 col-sm-4 col-lg-4 product-block", ""));
+               // Console.WriteLine(item.GetAttributeValue("col-xs-6 col-md-4 col-sm-4 col-lg-4 product-block", ""));
 
                 Console.WriteLine(item.Descendants("div")
                     .Where(node => node.GetAttributeValue("class", "")
                     .Equals("product")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t'));
 
-
-                pageNr++; Console.WriteLine(item.Descendants("div")
-                    .Where(node => node.GetAttributeValue("class", "")
-                    .Equals("right text-right price")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t'));
-
+              //  ++pageNr;
             }
+
+            Console.WriteLine($"Count of dresses: {productList.Count}.");
 
             Console.WriteLine();
         }
 
+
+       
     }
+
+
 }
