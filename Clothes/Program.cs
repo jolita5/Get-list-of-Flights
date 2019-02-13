c7edc98f53db47313f654a1d7975201014ba0f78
DESKTOP-6KJ0KT3\Jolita <jolita.jezukeviciute@gmail.com>
2019-02-13 21:26:42 +0200

    List of clothes gotten. It looks good but only 60 items

--- Clothes/Program.cs
+++ Clothes/Program.cs
@@ -14,9 +14,10 @@ namespace Flights
         static void Main(string[] args)
         {
 
+            int pageNr = 0;
+            GetHtmlAsync(pageNr);
 
-            GetHtmlAsync();
-
+            //  GetRequest(GetUrl(pageNr));
 
 
             Console.ReadLine();
@@ -31,7 +32,7 @@ namespace Flights
 
             if (pageNr != 0)
             {
-                return url = "https://newmood.lt/moterims/products?page" + pageNr + "sort=-latest&new=true&ap=1%2C47";
+                return url = "https://newmood.lt/moterims/products?page=" + pageNr + "&sort=-latest&ap=1%2C47&new=true";
             }
 
 
@@ -42,16 +43,22 @@ namespace Flights
 
 
 
-        private static async void GetHtmlAsync()
+        private static async void GetHtmlAsync(int pageNr)
         {
 
-            int pageNr = 0;
+            string url = "https://newmood.lt/moterims/products?sort=-latest&new=true&ap=1%2C47";
+
+
+            if (pageNr != 0)
+            {
+                url = "https://newmood.lt/moterims/products?page=" + pageNr + "&sort=-latest&ap=1%2C47&new=true";
+            }
 
             var raknings = GetUrl(pageNr);
 
 
             var httpClient = new HttpClient();
-            var html = await httpClient.GetStringAsync(GetUrl(pageNr));
+            var html = await httpClient.GetStringAsync(url);
 
             var htmlDocument = new HtmlDocument();
             htmlDocument.LoadHtml(html);
@@ -62,27 +69,45 @@ namespace Flights
             .Where(node => node.GetAttributeValue("id", "")
             .Equals("new-product-list")).ToList();
 
-
             var productList = htmlDocument.DocumentNode.Descendants("div")
            .Where(node => node.GetAttributeValue("class", "")
            .Contains("col-xs-6 col-md-4 col-sm-4 col-lg-4 product-block")).ToList();
 
-
-            //while (productList.Count > 0)
-            //{
+            while (productList.Count > 0)
+            { 
             foreach (var item in productList)
             {
-                Console.WriteLine(item.GetAttributeValue("col-xs-6 col-md-4 col-sm-4 col-lg-4 product-block", ""));
-
-                Console.WriteLine(item.Descendants("div")
-                    .Where(node => node.GetAttributeValue("class", "")
-                    .Equals("product")).FirstOrDefault().InnerText.Trim('\n'));
 
                 raknings = GetUrl(++pageNr);
 
-                // }
+                Console.WriteLine(item.GetAttributeValue("brand", ""));
+
+                Console.WriteLine(
+                    item.Descendants("div")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("title")).FirstOrDefault().InnerText.Trim('\r', '\n', 't'));
+
+                Console.WriteLine(
+                    Regex.Match(
+                        item.Descendants("div")
+                        .Where(node => node.GetAttributeValue("class", "")
+                        .Equals("right text-right price")).FirstOrDefault().InnerText.Trim('\r', '\n', 't')
+                    , @"\d+.\d+"));
+
+                    //Console.WriteLine(
+                    //    item.Descendants("div")
+                    //    .Where(node => node.GetAttributeValue("class", "")
+                    //    .Equals("size")).FirstOrDefault().InnerText.Trim('\r', '\n', 't'));
+
+                    Console.WriteLine(item.Descendants("a").FirstOrDefault().GetAttributeValue("href", ""));
+                
+                    ++pageNr;
+
+
+                }
 
             }
+
 
 
 
@@ -94,6 +119,20 @@ namespace Flights
 
         }
 
+        static async void GetRequest(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string myContent = await content.ReadAsStringAsync();
+                        Console.WriteLine(myContent);
+                    }
+                }
+            }
+        }
 
     }
 
